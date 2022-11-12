3412ddf9cf678b1904559e3dbf89a57bef985ad3
rhulk <quique1904@gmail.com>
2022-11-12 20:28:02 +0100

    Inicial

---
+++ TempoRailWay/src/main/java/pack/TempoRailWayApplication.java
@@ -0,0 +1,14 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempoRailWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempoRailWayApplication.class, args);
+		System.out.println(" Hola Mundo Rail Way");
+	}
+
+}
